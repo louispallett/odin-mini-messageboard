cd9ec73f51b24d4783a2c20bd287492d678c9941
Louis Pallett <louis.pallett@outlook.com>
2024-03-29 15:22:53 +0000

    Add files post deployment

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+const formatDate = require("../public/javascripts/format-date")
+
+const express = require('express');
+const router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "LowPal",
+    added: formatDate(new Date()),
+  },
+  {
+    text: "I'm using NodeJS!",
+    user: "Jeffrey Archer",
+    added: formatDate(new Date()),
+  },
+];
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+router.get("/new", (req, res, next) => {
+  res.render("form", { title: "New Message" })
+});
+
+router.post("/new", (req, res) => {
+  messages.push(
+    {
+      text: req.body.message,
+      user: req.body.name,
+      added: formatDate(new Date()),
+    }
+  );
+  res.redirect("/");
+});
+
+module.exports = router;
